2c4bdb2498895a3a8374dce83ab9bb68408e1dfe
RBLX Dev <83198770+Masoncubs@users.noreply.github.com>
2022-02-12 19:26:52 -0500

    Initial commit

---
+++ handlers/slash_handler.js
@@ -0,0 +1,79 @@
+const {
+  Client,
+  MessageActionRow,
+  MessageButton,
+  MessageEmbed,
+  Guild,
+} = require("discord.js");
+const fs = require("fs");
+const ee = require("../settings/embed.json");
+const emoji = require("../settings/emoji.json");
+
+/**
+ *
+ * @param {Client} client
+ */
+
+module.exports = async (client) => {
+  try {
+    client.arrayOfcommands = [];
+    fs.readdirSync("./commands").forEach((cmd) => {
+      let commands = fs
+        .readdirSync(`./commands/${cmd}/`)
+        .filter((file) => file.endsWith(".js"));
+      for (cmds of commands) {
+        let pull = require(`../commands/${cmd}/${cmds}`);
+        if (pull.options) {
+          pull.options
+            .filter((g) => g.type === "SUB_COMMAND")
+            .forEach((sub) => {
+              client.subcmd.set(sub.name, sub);
+            });
+        }
+        if (pull.name) {
+          client.commands.set(pull.name, pull);
+          client.arrayOfcommands.push(pull);
+        } else {
+          continue;
+        }
+      }
+    });
+    client.on("ready", async () => {
+      try {
+        // await client.guilds.cache
+        //   .get(`YOUR_GUILD_ID`)
+        //   .commands.set(client.arrayOfcommands);
+        await client.application.commands
+          .set(client.arrayOfcommands)
+          .then((s) => {
+            console.log("Successfully reloaded application (/) commands.");
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      } catch (e) {
+        console.log(e);
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
+  /**
+   *
+   * @param {CommandInteraction} interaction
+   * @param {String} data
+   */
+  client.embed = (interaction, data) => {
+    return interaction.followUp({
+      embeds: [
+        new MessageEmbed()
+          .setColor(ee.color)
+          .setTitle(data.substr(0, 2000))
+          .setFooter({
+            text: ee.footertext,
+            iconURL: ee.footericon,
+          }),
+      ],
+    });
+  };
+};
